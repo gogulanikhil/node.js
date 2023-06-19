4439da18a375d45d4ae45fccb394019d332a89db
G Nikhil <gogulanikhil@gmail.com>
2023-06-19 18:45:35 +0530

    es2015

---
+++ main.mjs
@@ -0,0 +1,7 @@
+// import * as math from "./math-esm.mjs"
+import {add,subtract} from "./math-esm.mjs"
+
+// const [add,subtract]=math 
+
+console.log(add(4,5));
+console.log(subtract(4,5));
---
+++ math-esm.mjs
@@ -0,0 +1,29 @@
+// const add=(a,b)=>{
+
+//     return a+b;
+// }
+// export default add
+
+
+// in line export
+// const add =(a,b)=>{
+//     return a+b;
+// }
+// const subtract=(a,b)=>{
+//     return a-b
+// }
+
+// export default {
+//     add,
+//     subtract
+// }
+
+// we can write export at the time of function only
+
+export const add=(a,b)=>{
+    return a+b
+}
+
+export const subtract=(a,b)=>{
+    return a-b
+}
